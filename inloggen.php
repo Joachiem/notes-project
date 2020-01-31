d0c47bab52e00383a9d51755d81a9c3aa4cd2992
JasperN038 <59870235+JasperN038@users.noreply.github.com>
2020-01-31 12:10:11 +0100

    Add files via upload

--- inloggen.php
+++ inloggen.php
@@ -7,13 +7,22 @@ if (!$conn) {
 }
 ?>
 
-<form method="post" action="server_inloggen.php">
-  <br>
-  Gebruikersnaam:<br>
-  <input type="text" name="user_gn">
-  <br>
-  Wachtwoord:<br>
-  <input type="password" name="user_ww">
-  <br><br>
-  <input type="submit" value="Inloggen">
-</form>
+<!DOCTYPEhtml>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/main.css">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Notes.</h1>
+			<form method="post" action="server_inloggen.php">
+				<br>Gebruikersnaam:<br>
+				<input type="text" name="user_gn">
+				<br>Wachtwoord:<br>
+				<input type="password" name="user_ww">
+				<br><br>
+				<input type="submit" value="Inloggen">
+			</form>
+		</div>	
+	</body>	
+</html>
