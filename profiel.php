1e0c6f64412628582dba7e97d0498b9af579c1d9
JasperN038 <59870235+JasperN038@users.noreply.github.com>
2020-02-04 11:47:33 +0100

    Add files via upload

--- profiel.php
+++ profiel.php
@@ -5,27 +5,34 @@ $conn = mysqli_connect($dbServer, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("De MySQL server kon niet bereikt worden.");
 }
-if (!$_SESSION['loggedin'] == TRUE) {
-	header('Location: inloggen.php');
-} else {
+?>
+<!DOCTYPEhtml>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/reset.css">
+		<link rel="stylesheet" type="text/css" href="style/main.css">
+		<script src="scripts/darkmode.js"></script>
+	</head>
+	<body class="dark-mode">
+		<?php
+		if (!$_SESSION['loggedin'] == TRUE) {
+			header('Location: inloggen.php');
+		} else {
+			$stmt = $conn->prepare('SELECT gebruikersnaam FROM gebruikers WHERE user_id = ?');
+			$stmt->bind_param('s', $_SESSION['user_id']);
+			$stmt->execute();
+			$stmt->store_result();
+			$stmt->bind_result($gebruikersnaam);
+			$stmt->fetch();
 
-	$stmt = $conn->prepare('SELECT gebruikersnaam FROM gebruikers WHERE user_id = ?');
-	$stmt->bind_param('s', $_SESSION['user_id']);
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($gebruikersnaam);
-	$stmt->fetch();
-
-echo "Account-ID: " . $_SESSION['user_id'];
-echo "<br> Gebruikersnaam: " . $gebruikersnaam;
-
-
-} ?>
-
-<br>
-<form action="change_ww.php">
-    <input type="submit" value="Verander wachtwoord" />
-</form>
-
-
-
+			echo "Account-ID: " . $_SESSION['user_id'];
+			echo "<br> Gebruikersnaam: " . $gebruikersnaam;
+		}
+		?>
+		<br>
+		<form action="change_ww.php">
+			<div class="form-item">
+				<input class="verander-wachtwoord" type="submit" value="VERANDER WACHTWOORD" />
+			</div>	
+		</form>
+</html>
