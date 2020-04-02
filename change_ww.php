896bdd59b3c13d46af987d17e3356541883132d3
JasperN038 <59870235+JasperN038@users.noreply.github.com>
2020-04-02 18:30:43 +0200

    Add files via upload

--- change_ww.php
+++ change_ww.php
@@ -31,6 +31,7 @@ $stmt = $conn->prepare('SELECT gebruikersnaam FROM gebruikers WHERE user_id = ?'
 		<ul class="navbar">
 			<li><a href="my_notes.php">Mijn Notities</a><li>
 			<li><a href="profiel.php">Mijn Profiel</a></li>
+			<li id="right"><a href="uitloggen.php">Uitloggen</a></li>
 		</ul>
 		<button type="button" class="darkmode" name="dark_light" onclick="toggleDarkLight()" title="Toggle dark/light mode">🌛</button>
 		<form method="post" action="server_change_ww.php">
